83b70550313cb61fd74d5d769aaa114b0150a8d1
ReynanSantts <reynanmesquita016@gmail.com>
2025-05-27 19:07:04 -0300

    fix: recebido merge

--- backend/routes/pagamento.js
+++ backend/routes/pagamento.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.post('/create-payment-intent', async (req, res) => {
   const { valor } = req.body;
